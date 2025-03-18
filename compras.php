a9f6b3f56c5f301b7f8da30745cdb8cb7a77fab6
teodwro <teogreselle@gmail.com>
2025-03-18 08:40:16 -0300

    commit

--- compras.php
+++ compras.php
@@ -1,7 +1,13 @@
 <?php
 
 require_once('twig_carregar.php');
+require('inc/banco.php');
+
+$dados = $pdo->query('SELECT * FROM compras');
+
+$comp = $dados->fetchAll(PDO::FETCH_ASSOC);
 
 echo $twig->render('compras.html', [
-    'titulo' => 'Compras'
+    'titulo' => 'Compras',
+    'compras' => $comp,
 ]);
