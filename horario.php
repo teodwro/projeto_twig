c0a5ad731bbe135269543eeb34a98a81b15d4d37
teodwro <teogreselle@gmail.com>
2025-03-25 08:45:06 -0300

    Update horario.php

--- horario.php
+++ horario.php
@@ -7,10 +7,11 @@ date_default_timezone_set('America/Sao_Paulo');
 
 $data = Carbon::now();
 $data_amanha = Carbon::now()->addDay(1);
-$data_formatada = $data->format()
+$data_formatada = $data->format('d-m-y H:i:s');
+$data_amanha_formatada = $data_amanha->format('d-m-y H:i:s');
 
 echo $twig->render('horario.html', [
     'titulo' => 'Horario',
-    'data' => $data,
-    'amanha' => $data_amanha
+    'data' => $data_formatada,
+    'amanha' => $data_amanha_formatada
 ]);
