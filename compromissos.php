e837d784406f2e2bec70d3c5a4b7102199c5be34
teodwro <teogreselle@gmail.com>
2025-04-10 11:08:02 -0300

    Ordenar

--- compromissos.php
+++ compromissos.php
@@ -3,7 +3,13 @@
 require_once('twig_carregar.php');
 require('inc/banco.php');
 
-$dados = $pdo->query('SELECT * FROM compromissos');
+$ordem = $_POST['ordem'] ?? null; 
+
+if ($ordem == 'proximo') {
+    $dados = $pdo->query('SELECT * FROM compromissos ORDER BY ABS(DATEDIFF(data, CURDATE())) ASC');
+} else {
+    $dados = $pdo->query('SELECT * FROM compromissos');
+} 
 
 $comp = $dados->fetchAll(PDO::FETCH_ASSOC);
 
